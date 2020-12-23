9bccc6c187f156f7258e550a039ad19a6b4af867
Andrew Doepke <andrewdoepke@outlook.com>
2020-12-22 19:14:25 -0600

    Initial Commit

---
+++ Shut Up ButtonTests/Shut_Up_ButtonTests.swift
@@ -0,0 +1,34 @@
+//
+//  Shut_Up_ButtonTests.swift
+//  Shut Up ButtonTests
+//
+//  Created by Andrew Doepke on 12/22/20.
+//  Copyright © 2020 Andrew Doepke. All rights reserved.
+//
+
+import XCTest
+@testable import Shut_Up_Button
+
+class Shut_Up_ButtonTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
